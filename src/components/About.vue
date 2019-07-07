c30b2918eff7e0553f77410d1c436d42a2053a27
Karthik <venkateshkarthik42@gmail.com>
2019-07-06 22:37:37 -0500

    First commit

---
+++ src/components/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>This is {{ their_name }}  To Do list developed in Vue.js to learn Vue.js</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data(){
+    return{
+      
+    their_name: this.$route.params.name
+
+
+
+      
+    
+    }
+
+  }
+ 
+}
+</script>
